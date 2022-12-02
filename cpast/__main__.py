bb444115cc5c366d3e4e7aa989f1f511a732a95f
skuzow <alex@skuzow.net>
2022-12-02 21:57:26 +0100

    feat: commit past

--- cpast/__main__.py
+++ cpast/__main__.py
@@ -1,5 +1,23 @@
-from cpast.cpast import init
+import sys
+from argparse import ArgumentParser
 
+from cpast.cpast import run
+
+
+parser = ArgumentParser(prog='cpast', description='Cpast CLI')
+parser.add_argument('-d', '--date',
+                    help='Commit date, example: 2021-12-25',
+                    required=True, type=str, default=None)
+parser.add_argument('-t', '--time',
+                    help='Commit time, example: 22:13:05',
+                    required=True, type=str, default=None)
+parser.add_argument('-m', '--message',
+                    help='Commit message, example: "init :)"',
+                    required=True, type=str, default=None)
 
 if __name__ == '__main__':
-    print(init())
+    if len(sys.argv) == 1:
+        parser.print_help()
+    else:
+        args = parser.parse_args()
+        run(args.date, args.time, args.message)
--- tests/test_cpast.py
+++ tests/test_cpast.py
@@ -1,12 +1,24 @@
+import os
 import unittest
 
-from cpast.cpast import init
+from cpast.cpast import format_date, prepare_vars
+
+
+DATE: str = '2021-12-25'
+TIME: str = '22:13:05'
+FORMATTED_DATE: str = '"2021-12-25T22:13:05"'
 
 
 class TestSimple(unittest.TestCase):
 
-    def test_init(self):
-        self.assertEqual(init(), 'hello from gitpast :)')
+    def test_format_date(self):
+        formatted_date: str = format_date(DATE, TIME)
+        self.assertEqual(formatted_date, FORMATTED_DATE)
+
+    def test_prepare_vars(self):
+        prepare_vars(FORMATTED_DATE)
+        self.assertEqual(os.environ['GIT_AUTHOR_DATE'], FORMATTED_DATE)
+        self.assertEqual(os.environ['GIT_COMMITTER_DATE'], FORMATTED_DATE)
 
 
 if __name__ == '__main__':
