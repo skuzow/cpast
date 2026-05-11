219e131007f557f62bcaa69bdf631d23a8b9f034
skuzow <alex@skuzow.net>
2026-05-11 19:25:57 +0200

    feat(tests): improve class namings

--- tests/test_check.py
+++ tests/test_check.py
@@ -12,7 +12,7 @@ BROKEN_TIME: str = '-1:13:05'
 OVERPASSED_TIME: str = '24:13:05'
 
 
-class TestSimple(unittest.TestCase):
+class TestCheck(unittest.TestCase):
 
     def test_check_date(self):
         self.assertEqual(check_date(DATE), True)
